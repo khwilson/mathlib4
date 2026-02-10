c9c935e0f05e130a6fd886163a99945f213a774b
Kevin Wilson <khwilson@gmail.com>
2026-02-10 14:56:48 -0500

    Update Mathlib/Analysis/Complex/Circle.lean
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- Mathlib/Analysis/Complex/Circle.lean
+++ Mathlib/Analysis/Complex/Circle.lean
@@ -136,8 +136,8 @@ def expHom : ℝ →+ Additive Circle where
 
 @[simp] lemma exp_sub (x y : ℝ) : exp (x - y) = exp x / exp y := expHom.map_sub x y
 @[simp] lemma exp_neg (x : ℝ) : exp (-x) = (exp x)⁻¹ := expHom.map_neg x
-@[simp] lemma exp_nsmul (x : ℝ) (n : ℕ) : exp (n • x) = exp (x) ^ n := expHom.map_nsmul x n
-@[simp] lemma exp_zsmul (x : ℝ) (n : ℤ) : exp (n • x) = exp (x) ^ n := expHom.map_zsmul x n
+@[simp] lemma exp_nsmul (x : ℝ) (n : ℕ) : exp (n • x) = exp x ^ n := expHom.map_nsmul x n
+@[simp] lemma exp_zsmul (x : ℝ) (z : ℤ) : exp (z • x) = exp x ^ z := expHom.map_zsmul x z
 
 lemma exp_pi_ne_one : Circle.exp Real.pi ≠ 1 := by
   intro h
