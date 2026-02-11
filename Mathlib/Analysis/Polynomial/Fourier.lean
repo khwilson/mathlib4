df912ecd006fa02ef85f6eb17e068f1ef8e37680
Kevin H Wilson <khwilson@gmail.com>
2026-02-11 06:06:39 -1000

    final name fixes

--- Mathlib/Analysis/Polynomial/Fourier.lean
+++ Mathlib/Analysis/Polynomial/Fourier.lean
@@ -13,21 +13,21 @@ public import Mathlib.Topology.Algebra.Polynomial
 /-!
 # Fourier Coefficients of Polynomials
 
-We define a linear map from `ℂ[X]` to the `MeasureTheory.Lp` (with `p := 2`) space on the additive
+We define an algebra map from `ℂ[X]` to the `MeasureTheory.Lp` (with `p := 2`) space on the additive
 circle and show that it sends monomials to the Fourier basis. From this, we derive that polynomial
 coefficients match Fourier coefficients and prove Parseval's identity for polynomials.
 
 ## Main definitions
 
-- `Polynomial.toAddCircle`: Linear map from `ℂ[X]` to `C(AddCircle (2 * π), ℂ)` that evaluates
+- `Polynomial.toAddCircle`: Algebra map from `ℂ[X]` to `C(AddCircle (2 * π), ℂ)` that evaluates
   polynomials on the unit circle.
 
 ## Main results
 
-- `Polynomial.toAddCircle_fourierCoeff_zcoeff`: The `n`-th Fourier coefficient of a polynomial
+- `Polynomial.fourierCoeff_toAddCircle`: The `n`-th Fourier coefficient of a polynomial
   equals its `n`-th coefficient when `n` is nonnegative, else 0.
-- `Polynomial.toAddCircle_fourierCoeff_coeff`: A variant of
-  `Polynomial.toAddCircle_fourierCoeff_zcoeff` for `ℕ` arguments.
+- `Polynomial.fourierCoeff_toAddCircle_natCast`: A variant of `Polynomial.fourierCoeff_toAddCircle`
+  for `ℕ` arguments.
 - `Polynomial.sum_sq_norm_coeff_eq_circleAverage`: Parseval's identity that the sum of the squares
   of the norms of the coefficients equals the average over the circle of the norm square.
 
@@ -44,7 +44,7 @@ variable (p : ℂ[X])
 
 local instance instTwoPiPos : Fact (0 < 2 * π) := Fact.mk Real.two_pi_pos
 
-/-- Linear map from `ℂ[X]` to `C(AddCircle (2 * π), ℂ)` that evaluates polynomials on the unit
+/-- Algebra map from `ℂ[X]` to `C(AddCircle (2 * π), ℂ)` that evaluates polynomials on the unit
 circle. For a polynomial `p`, this maps it to the function `fun θ ↦ p (exp (I * θ))`. -/
 noncomputable def toAddCircle : ℂ[X] →ₐ[ℂ] C(AddCircle (2 * π), ℂ) :=
   Polynomial.aeval { toFun c := c.toCircle }
@@ -67,7 +67,9 @@ theorem toAddCircle_monomial_eq_smul_fourier {n : ℕ} {c : ℂ} :
     (monomial n c).toAddCircle = c • fourier n := by
   ext θ; simp [toAddCircle, AddCircle.toCircle_nsmul]
 
-theorem toAddCircle_fourierCoeff_zcoeff (n : ℤ) :
+/-- The `n`th Fourier coefficient of a polynomial is the coefficient of `X ^ n`, or
+zero if `n < 0`. -/
+theorem fourierCoeff_toAddCircle (n : ℤ) :
     fourierCoeff (T := 2 * π) p.toAddCircle n = if 0 ≤ n then p.coeff n.natAbs else 0 := by
   have : n < 0 ∨ ∃ k : ℕ, n = k := lt_or_ge n 0 |>.imp_right Int.eq_ofNat_of_zero_le
   induction p using Polynomial.induction_on' with obtain (hn | ⟨k, rfl⟩) := this
@@ -76,22 +78,23 @@ theorem toAddCircle_fourierCoeff_zcoeff (n : ℤ) :
   | monomial m a =>
     simp_all [not_le_of_gt, coeff_monomial, toAddCircle_monomial_eq_smul_fourier,
       fourierCoeff.const_smul, fourierCoeff_fourier, Pi.single_apply]
-    try grind
+    grind
 
-theorem toAddCircle_fourierCoeff_coeff (n : ℕ) :
+/-- The `n`th Fourier coefficient of a polynomial is the coefficient of `X ^ n` -/
+theorem fourierCoeff_toAddCircle_natCast (n : ℕ) :
     fourierCoeff (T := 2 * π) p.toAddCircle n = p.coeff n := by
-  simp [toAddCircle_fourierCoeff_zcoeff]
+  simp [fourierCoeff_toAddCircle]
 
-theorem toAddCircle_fourierCoeff_eq_zero_of_lt_zero (n : ℤ) (hn : n < 0) :
+theorem fourierCoeff_toAddCircle_eq_zero_of_lt_zero (n : ℤ) (hn : n < 0) :
     fourierCoeff (T := 2 * π) p.toAddCircle n = 0 := by
-  simp [toAddCircle_fourierCoeff_zcoeff, hn]
+  simp [fourierCoeff_toAddCircle, hn]
 
 /-- **Parseval's Identity** for polynomials -/
 theorem sum_sq_norm_coeff_eq_circleAverage : ∑ i ∈ p.support, ‖p.coeff i‖ ^ 2 =
     Real.circleAverage (fun θ ↦ ‖p.eval θ‖ ^ 2) 0 1 := by
   -- Rewrite coefficients as Fourier coefficients
   have := tsum_sq_fourierCoeff (T := 2 * π) (p.toAddCircle.toLp 2 haarAddCircle ℂ)
-  simp_rw [fourierCoeff_toLp, toAddCircle_fourierCoeff_zcoeff] at this
+  simp_rw [fourierCoeff_toLp, fourierCoeff_toAddCircle] at this
   rw [tsum_eq_sum (s := p.support.map ⟨_, Nat.cast_injective⟩) (fun b hb => ?eq_zero)] at this
   case eq_zero =>
     obtain h | h := le_or_gt 0 b
